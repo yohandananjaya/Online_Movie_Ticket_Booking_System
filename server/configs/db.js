7e89f2e7ccf029366f995df3baa01d47c347fd3b
yohandananjaya <ydananjaya47@gmail.com>
2025-09-19 16:36:48 +0530

    hee

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected', () => console.log('MongoDB connected successfully'));
-        await mongoose.connect(`${process.env.MONGODB_URI}/showtixx`,)
+        await mongoose.connect(`${process.env.MONGODB_URI}/showtix`,)
     } catch (error) {
         console.log(error.message);
     }
