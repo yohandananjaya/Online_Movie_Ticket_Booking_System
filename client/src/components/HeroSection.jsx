d6cef95f54e0f083085f06b4134cc6fb609d39ee
W.G.Y.D. Premalal <ydananjaya47@gmail.com>
2026-02-24 00:27:16 +0530

    Testing auto build pipeline2

--- client/src/components/HeroSection.jsx
+++ client/src/components/HeroSection.jsx
@@ -16,7 +16,7 @@ const HeroSection = () => {
         <img src={assets.marvelLogo} alt="" className="max-h-11 lg:h-11 mt-20"/>
 
         <h1 className='text-5xl md:text-[70px] md:leading-18 font-semibold max-w-110'>
-            Deadpool <br/> & Wolverine
+            BatMan <br/> & Wolverine
         </h1>
 
         <div className='flex items-center gap-4 text-gray-300'>
